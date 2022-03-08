a5028e0be45d7ca660fde33b754f300408fd3027
Ananpiya134 <anan.piyapanee@gmail.com>
2022-03-09 03:37:02 +0700

    commit before revert to get teamprofile and user profile

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,11 +1,13 @@
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function NavBar() {
+	const { user } = useContext(AuthContext);
 	const [search, setSearch] = useState("");
 
 	const navigate = useNavigate();
-
+	console.log(user);
 	return (
 		<div className="NavBar_container">
 			<div className="NavBar_left ">
@@ -41,17 +43,27 @@ function NavBar() {
 						onChange={(e) => setSearch(e.target.value)}
 					/>
 				</div>
-				<button className="btn mx-3 d-flex friend_btn">
+				<button
+					className="btn mx-3 d-flex friend_btn"
+					onClick={() => navigate("/team-profile")}
+				>
 					<i className="fa-solid fa-user-group me-2 text-white" />
-					friend
+					Team
 				</button>
 				<div className="NavBar_profile d-flex align-items-center">
-					<div className="NavBar_profile_img ms-2">
+					<div
+						className="NavBar_profile_img ms-2"
+						onClick={() => navigate("/user")}
+					>
 						<i className="fa-solid fa-user" />
 					</div>
 
-					<b style={{ fontSize: "14px" }} className="font-secondary mx-3">
-						Oakleyster
+					<b
+						style={{ fontSize: "14px" }}
+						className="font-secondary mx-3"
+						onClick={() => navigate("/user")}
+					>
+						{user.username}
 					</b>
 					<div className="vl"></div>
 					<button className="btn btn-secondary profile_btn">
